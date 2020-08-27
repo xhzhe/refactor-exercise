193e52e5d1853999158ffa7e6705c717d6924951
alex <alex.h.z.xu@oocl.com>
2020-08-27 14:57:29 +0800

    [alex] refactor: extract calculate amount to function

--- src/statement.js
+++ src/statement.js
@@ -9,24 +9,7 @@ function statement(invoice, plays) {
     const format = USDFormat();
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-        switch (play.type) {
-            case TRAGEDY:
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case COMEDY:
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = generateAmount(play.type, perf.audience);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
@@ -38,6 +21,28 @@ function statement(invoice, plays) {
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits \n`;
     return result;
+}
+
+function generateAmount(type, audience) {
+    let thisAmount = 0;
+    switch (type) {
+        case TRAGEDY:
+            thisAmount = 40000;
+            if (audience > 30) {
+                thisAmount += 1000 * (audience - 30);
+            }
+            break;
+        case COMEDY:
+            thisAmount = 30000;
+            if (audience > 20) {
+                thisAmount += 10000 + 500 * (audience - 20);
+            }
+            thisAmount += 300 * audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${type}`);
+    }
+    return thisAmount;
 }
 
 function USDFormat() {
