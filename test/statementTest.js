36317e5e66c8022926d75a02113f9e1c11775757
alex <alex.h.z.xu@oocl.com>
2020-08-27 15:44:28 +0800

    [alex] feature: add test to test html generator

--- test/statementTest.js
+++ test/statementTest.js
@@ -171,3 +171,37 @@ test('Sample test', t => {
         'Amount owed is $400.00\n' +
         'You earned 0 credits \n');
 });
+
+test('Sample test', t => {
+    //given
+    const invoice = {
+        'customer': 'BigCo',
+        'performances': [
+            {
+                'playID': 'hamlet',
+                'audience': 55,
+            },
+            {
+                'playID': 'as-like',
+                'audience': 35,
+            },
+            {
+                'playID': 'othello',
+                'audience': 40,
+            },
+        ],
+    };
+    //when
+    const result = statement(invoice, plays, 'html');
+    //then
+    t.is(result, '<h1>Statement for BigCo</h1>\n' +
+        '<table>\n' +
+        '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
+        ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
+        ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
+        ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
+        '</table>\n' +
+        '<p>Amount owed is <em>$1,730.00</em></p>\n' +
+        '<p>You earned <em>47</em> credits</p>\n');
+
+});
