1cbbbedea14621b36e96c8cbe39ffcba107ea24a
alex <alex.h.z.xu@oocl.com>
2020-08-27 14:46:27 +0800

    [alex] refactor: extract magic string to constant

--- src/statement.js
+++ src/statement.js
@@ -1,3 +1,7 @@
+const TRAGEDY = 'tragedy';
+
+const COMEDY = 'comedy';
+
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -7,13 +11,13 @@ function statement(invoice, plays) {
         const play = plays[perf.playID];
         let thisAmount = 0;
         switch (play.type) {
-            case 'tragedy':
+            case TRAGEDY:
                 thisAmount = 40000;
                 if (perf.audience > 30) {
                     thisAmount += 1000 * (perf.audience - 30);
                 }
                 break;
-            case 'comedy':
+            case COMEDY:
                 thisAmount = 30000;
                 if (perf.audience > 20) {
                     thisAmount += 10000 + 500 * (perf.audience - 20);
@@ -26,7 +30,7 @@ function statement(invoice, plays) {
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if (COMEDY === play.type) volumeCredits += Math.floor(perf.audience / 5);
         //print line for this order
         result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
